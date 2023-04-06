9d46c6a8d6390b7f0f017ab0370297c601d8975c
tcs-git <thiagocavalcantesimonato@hotmail.com>
2023-04-06 16:14:09 -0400

    Handling of the "IfHeaderExists" statement.

--- R/processReadStatement.R
+++ R/processReadStatement.R
@@ -1,9 +1,16 @@
 processReadStatement = function(s) {
   if (grepl(".* from file .* header \"*\"",
             s$command)) {
-    words = strsplit(s$command, " ")[[1]]
+    if (grepl("ifheaderexists", s$command)) {
+      words = strsplit(s$command, " ")[[1]]
+      header = paste0(words[5], "$", gsub("\"", "", words[7]))
+      command = sprintf('%s[] = %s$`%s`', words[2], words[5], gsub("\"", "", words[7]))
+      toRet = paste0("if(!is.null(", header, ")){", command, "}")
 
-    return(sprintf('%s[] = %s$`%s`', words[1], words[4], gsub("\"", "", words[6])))
+    } else {
+      words = strsplit(s$command, " ")[[1]]
+      return(sprintf('%s[] = %s$`%s`', words[1], words[4], gsub("\"", "", words[6])))
+    }
   }
-
+  
 }
